5abb2a79af17dd9a97d2c449948f02dd1a64dde9
Shal <Shal.work@mail.ru>
2020-11-17 16:59:37 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+//'use strict';
+
+a = 5;
+console.log(a);
+
+//let answer1 = +prompt("Усть ли Вам 18?", "Да");
+//console.log(typeof(answer1)); //превращение в число
+
+console.log("arr" + "-object");
+console.log(4 + "-object");
+let i = "40";
+console.log(typeof("i"));
+console.log(typeof(+"i"));
+
+let a = 10;
+
+console.log(a++); 
+console.log( ++a);  
+
+console.log(50%2);
+ 
+console.log(" 2 " == 2);
+console.log(" 2 " === 2);
+
+
+let isChecket = true, 
+      isClose = false;
+console.log(isChecket && isClose);
+
+
